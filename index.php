88a1109a0ec4319e4951d9e011eb349989e953e0
cacadosman <wetmanz23@gmail.com>
2017-07-19 23:20:24 +0700

    edit

--- index.php
+++ index.php
@@ -1,50 +1,78 @@
 <?php
 
-/**
- * Copyright 2016 LINE Corporation
- *
- * LINE Corporation licenses this file to you under the Apache License,
- * version 2.0 (the "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at:
- *
- *   https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
+require __DIR__ . '/vendor/autoload.php';
 
-require_once('./LINEBotTiny.php');
+use \LINE\LINEBot\SignatureValidator as SignatureValidator;
 
-$channelAccessToken = "TVnoo81tenPHZZgbEezn0VZJA1Dtk2LwFYZVON5KhjeUd+tOiTHO9oqjN8kfqD3aHzQNzSl6pX9dCCzlYWqTSd3O+lm7CEZq33wwCWGrz+rBgtvYuAg/6twleKAgI2WqiXKL5yBVbbgoD6DudAxZWAdB04t89/1O/w1cDnyilFU=';
-$channelSecret = '20825d4b30ae279025208ccc49b86d10';
+// load config
+$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv->load();
 
-$client = new LINEBotTiny($channelAccessToken, $channelSecret);
-foreach ($client->parseEvents() as $event) {
-    switch ($event['type']) {
-        case 'message':
-            $message = $event['message'];
-            switch ($message['type']) {
-                case 'text':
-                    $client->replyMessage(array(
-                        'replyToken' => $event['replyToken'],
-                        'messages' => array(
-                            array(
-                                'type' => 'text',
-                                'text' => $message['text']
-                            )
-                        )
-                    ));
-                    break;
-                default:
-                    error_log("Unsupporeted message type: " . $message['type']);
-                    break;
-            }
-            break;
-        default:
-            error_log("Unsupporeted event type: " . $event['type']);
-            break;
-    }
-};
+// initiate app
+$configs =  [
+	'settings' => ['displayErrorDetails' => true],
+];
+$app = new Slim\App($configs);
+
+/* ROUTES */
+$app->get('/', function ($request, $response) {
+	return "Lanjutkan!";
+});
+
+$app->post('/', function ($request, $response)
+{
+	// get request body and line signature header
+	$body 	   = file_get_contents('php://input');
+	$signature = $_SERVER['HTTP_X_LINE_SIGNATURE'];
+
+	// log body and signature
+	file_put_contents('php://stderr', 'Body: '.$body);
+
+	// is LINE_SIGNATURE exists in request header?
+	if (empty($signature)){
+		return $response->withStatus(400, 'Signature not set');
+	}
+
+	// is this request comes from LINE?
+	if($_ENV['PASS_SIGNATURE'] == false && ! SignatureValidator::validateSignature($body, $_ENV['CHANNEL_SECRET'], $signature)){
+		return $response->withStatus(400, 'Invalid signature');
+	}
+
+	// init bot
+	$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($_ENV['CHANNEL_ACCESS_TOKEN']);
+	$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $_ENV['CHANNEL_SECRET']]);
+
+	$data = json_decode($body, true);
+	foreach ($data['events'] as $event)
+	{
+		if ($event['type'] == 'message')
+		{
+			if($event['message']['type'] == 'text')
+			{
+				// send same message as reply to user
+				$result = $bot->replyText($event['replyToken'], $event['message']['text']);
+
+				// or we can use pushMessage() instead to send reply message
+				// $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($event['message']['text']);
+				// $result = $bot->pushMessage($event['source']['userId'], $textMessageBuilder);
+				
+				return $result->getHTTPStatus() . ' ' . $result->getRawBody();
+			}
+		}
+	}
+
+});
+
+// $app->get('/push/{to}/{message}', function ($request, $response, $args)
+// {
+// 	$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($_ENV['CHANNEL_ACCESS_TOKEN']);
+// 	$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $_ENV['CHANNEL_SECRET']]);
+
+// 	$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($args['message']);
+// 	$result = $bot->pushMessage($args['to'], $textMessageBuilder);
+
+// 	return $result->getHTTPStatus() . ' ' . $result->getRawBody();
+// });
+
+/* JUST RUN IT */
+$app->run();
