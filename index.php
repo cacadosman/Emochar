8f129e59f63527c23593d27e3199602e63a12337
cacadosman <wetmanz23@gmail.com>
2017-07-19 23:12:05 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Copyright 2016 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+require_once('./LINEBotTiny.php');
+
+$channelAccessToken = "TVnoo81tenPHZZgbEezn0VZJA1Dtk2LwFYZVON5KhjeUd+tOiTHO9oqjN8kfqD3aHzQNzSl6pX9dCCzlYWqTSd3O+lm7CEZq33wwCWGrz+rBgtvYuAg/6twleKAgI2WqiXKL5yBVbbgoD6DudAxZWAdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '20825d4b30ae279025208ccc49b86d10';
+
+$client = new LINEBotTiny($channelAccessToken, $channelSecret);
+foreach ($client->parseEvents() as $event) {
+    switch ($event['type']) {
+        case 'message':
+            $message = $event['message'];
+            switch ($message['type']) {
+                case 'text':
+                    $client->replyMessage(array(
+                        'replyToken' => $event['replyToken'],
+                        'messages' => array(
+                            array(
+                                'type' => 'text',
+                                'text' => $message['text']
+                            )
+                        )
+                    ));
+                    break;
+                default:
+                    error_log("Unsupporeted message type: " . $message['type']);
+                    break;
+            }
+            break;
+        default:
+            error_log("Unsupporeted event type: " . $event['type']);
+            break;
+    }
+};
